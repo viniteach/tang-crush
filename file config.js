050b867423a53a5c30c9800df2f66b32a9eb8e5f
viniteach <86723346+viniteach@users.noreply.github.com>
2021-07-01 15:34:34 +0700

    Update file config.js

--- file config.js
+++ file config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Bbi của tui à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Hii bae cuteee~!',
+    introDesc: `Bbi ơi tui có 1 câu hỏi em phải ns thật nha🥺`,
+    btnIntro: 'dạ hỏi i:33',
+    title: 'Cậu có yew tui hongggg 😙',
+    desc: 'M chưa trả lời mà thoát t bắt m zề nuôi áhh🔪😾�',
+    btnYes: 'Yew thì yêu <33333',
+    btnNo: 'Không ạ :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    btnReply: 'Gửi cho anh <33333',
+    reply: 'Yew hong có li do đc hong <33333333',
+    mess: 'Anh biết em yew anh mà 🥰. Yêu em nhiều nhiều baby của anhhh :333 �',
     messDesc: 'Yêu công chúa của anh 🥺.',
-    btnAccept: 'Uciiiii lun <3',
+    btnAccept: 'Dạaaaaaa <33333',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
